3cb4795b858d5f6d7def6359c47bb03b05e58bbe
CRESCO <murielberthoucrestey@gmail.com>
2017-06-06 15:26:02 +0200

    chgt models

--- app/models/poi.rb
+++ app/models/poi.rb
@@ -1,8 +1,7 @@
 class Poi < ApplicationRecord
   belongs_to :category
-  belongs_to :user
-  has many :reviews
+
+
   has_attachment :photo
 
-  validates :category, presence: true
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,7 +1,4 @@
 class Review < ApplicationRecord
   belongs_to :user
 
-  validates :poi, presence: true
-  validates :user, presence: true
-  validates :comment, length: {minimum: 10}
 end
