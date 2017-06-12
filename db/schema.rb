2d2958f6a4819e4c2799d71769b959c3ac93603c
CRESCO <murielberthoucrestey@gmail.com>
2017-06-12 20:32:15 +0200

    rating actualisé

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170612172753) do
+ActiveRecord::Schema.define(version: 20170612181912) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -68,6 +68,7 @@ ActiveRecord::Schema.define(version: 20170612172753) do
     t.integer  "stars"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "rating"
   end
 
   create_table "users", force: :cascade do |t|
